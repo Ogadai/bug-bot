e27eb1aa272a0649bac206e36688ca43881b381c
Ogadai <andy.lee.surfer@gmail.com>
2021-01-09 22:34:26 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,71 @@
-Controller.onChange(acBtn.DPad, function () {
-    if (Controller.pressed(acBtn.DUp)) {
+let commands: number[] = [];
+let running = false;
+
+Controller.onPressed(acBtn.DUp, function () {
+    commands.push(1);
+    music.playTone(294, music.beat(BeatFraction.Quarter));
+});
+Controller.onPressed(acBtn.DRight, function () {
+    commands.push(2);
+    music.playTone(523, music.beat(BeatFraction.Quarter));
+});
+Controller.onPressed(acBtn.DDown, function () {
+    commands.push(3);
+    music.playTone(450, music.beat(BeatFraction.Quarter));
+});
+Controller.onPressed(acBtn.DLeft, function () {
+    commands.push(4);
+    music.playTone(180, music.beat(BeatFraction.Quarter));
+});
+
+Controller.onPressed(acBtn.XB, function () {
+    commands = [];
+});
+
+Controller.onPressed(acBtn.XA, function () {
+    if (running) return;
+    running = true;
+    control.inBackground(function () {
+        for(let n = 0; n < commands.length; n++) {
+            doAction(commands[n]);
+        }
+        running = false;
+    });
+});
+
+function doAction(action: number): void {
+    let delayMs = 0;
+
+    if (action === 1) {
         servos.P1.run(100)
         servos.P2.run(-100)
-    } else if (Controller.pressed(acBtn.DDown)) {
+        delayMs = 1000;
+    } else if (action === 3) {
         servos.P1.run(-100)
         servos.P2.run(100)
-    } else if (Controller.pressed(acBtn.DLeft)) {
-        servos.P1.run(100)
-        servos.P2.run(100)
-    } else if (Controller.pressed(acBtn.DRight)) {
+        delayMs = 1000;
+    } else if (action === 2) {
         servos.P1.run(-100)
         servos.P2.run(-100)
-    } else {
-        servos.P1.stop()
-        servos.P2.stop()
+        delayMs = 1000;
+    } else if (action === 2) {
+        servos.P1.run(100)
+        servos.P2.run(100)
+        delayMs = 1000;
     }
-})
+
+    control.waitMicros(delayMs * 1000)
+}
+
 bluetooth.onBluetoothConnected(function () {
     DriveBit.setLedColor(0x00FF00)
     basic.showIcon(IconNames.Yes)
-})
-Controller.onPressed(acBtn.XB, function () {
-    music.playTone(294, music.beat(BeatFraction.Whole))
 })
 bluetooth.onBluetoothDisconnected(function () {
     DriveBit.setLedColor(0xFF0000)
     basic.showIcon(IconNames.No)
 })
-Controller.onPressed(acBtn.XX, function () {
-    music.playTone(523, music.beat(BeatFraction.Whole))
-})
+
 bluetooth.startTemperatureService()
 DriveBit.setLedColor(0x0000FF)
 basic.showIcon(IconNames.Happy)
